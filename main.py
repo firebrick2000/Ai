4580b2f0b7736a76f06dfbb34505b513edf6d064
Cohen-TT <>
2022-04-14 02:24:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+greetings = ["hello", "hi", "hola", "howdy", "greetings"]
+responses = ["music is so relaxing", "i love playing games", "i like pets. dogs are my favorite"]
+goodbyes = ["bye", "goodbye", "siya", "adios"]
+keyword = ["pet", "games", "music", ]
+print(random.choice(greetings))
+user = input("say something to start (or type bye to quit ) ")
+user = user.lower()
+while (user != "bye"):
+  keyword_found = False
+  
+  for index in range(len(keyword)):
+    if (keyword[index] in user):
+      print("bot: " + responses[index])
+      keyword_found = True
+
+  if (keyword_found == False):
+    new_keyword = input("i'm not sure how to respond. What keyword should i respond to? ")
+    keyword.append(new_keyword)
+    new_response = input("how should i respond to " +new_keyword+ "? ")
+    responses.append(new_response)
+  user = input("say something to start (or type bye to quit ")
+  user = user.lower()
+print(random.choice(goodbyes))
